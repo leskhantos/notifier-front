d6f62ad396f4d134b34944a496da99b52f79df67
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2020-11-02 16:16:29 +0600

    :chart_with_upwards_trend: Adding new pages.

---
+++ src/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-jumbotron header="Notifier Admin" lead="Админка для создания шаблонов">
+    <p>Для создания шаблона перейдите по кнопке</p>
+    <b-button variant="primary" href="/templates">Шаблоны</b-button>
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+  name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
