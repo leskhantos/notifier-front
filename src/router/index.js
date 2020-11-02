8e1194309651acc65b62711db95d47573bc60a77
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2020-11-02 16:14:59 +0600

    :building_construction: Making routing changes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import Template from '../Template.vue'
-import GlobalParameters from '../GlobalParameters.vue'
-import History from '../History.vue'
+import Template from '@/Template.vue'
+import GlobalParameters from '@/GlobalParameters.vue'
+import History from '@/History.vue'
+import Welcome from "@/Welcome";
 export default [
+    { path: '/', component: Welcome, name: "index" },
     { path: '/templates', component: Template, name: "templates" },
     { path: '/global-parameters', component: GlobalParameters, name: "globalParameters" },
-    { path: '/history', component: History, name: "History" }
+    { path: '/history', component: History, name: "history" }
 ]
