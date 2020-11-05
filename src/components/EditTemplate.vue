a68a458228a3d9905b7b32225aefc062012058c9
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2020-11-05 15:33:14 +0600

    :construction_worker: Adding template edit component.

---
+++ src/components/EditTemplate.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+          id="input-group-1"
+          label-for="input-1"
+      >
+        <b-form-input
+            class="mt-1"
+            id="input-1"
+            v-model="form.code"
+            required
+            placeholder="Код"
+        ></b-form-input>
+
+        <b-form-select
+            class="mt-1"
+            id="input-3"
+            v-model="form.delivery_channel"
+            :options="channels"
+            required
+        ></b-form-select>
+
+        <b-form-select
+            class="mt-1"
+            id="input-3"
+            v-model="form.language"
+            :options="languages"
+            required
+        ></b-form-select>
+        <b-form-textarea
+            class="mt-1"
+            id="textarea"
+            v-model="form.text"
+            placeholder="Контент"
+            rows="3"
+            max-rows="6"
+        ></b-form-textarea>
+
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditTemplate",
+  data() {
+    return {
+      form: {
+        code: '',
+        content: '',
+        delivery_channel: null,
+        language: null
+      },
+      languages: [{ text: 'Выберите язык', value: null }, 'Английский', 'Русский', 'Казахский'],
+      channels: [{ text: 'Выберите тип отправки', value: null }, 'SMS', 'PUSH', 'PUSH+SMS'],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Template.vue
+++ src/pages/Template.vue
@@ -1,25 +1,32 @@
 <template>
   <div>
+    <nav-bar/>
     <div class="row">
       <div class="col-10">
         <b-breadcrumb :items="breads" style="background-color: white"></b-breadcrumb>
       </div>
       <div class="col-2 p-1">
         <b-button variant="success"  v-b-modal.modal-1>Создать шаблон</b-button>
+        <b-button variant="warning"  v-b-modal.modal-2>Редактировать шаблон</b-button>
       </div>
     </div>
     <b-table striped hover :items="items" :fields="fields" responsive="responsive"></b-table>
     <b-modal id="modal-1" title="Создание шаблона">
-      <template-create-page/>
+      <create-template/>
+    </b-modal>
+    <b-modal id="modal-2" title="Редактирование шаблона">
+      <edit-template/>
     </b-modal>
   </div>
 </template>
 
 <script>
-import TemplateCreatePage from "@/components/TemplateCreatePage";
+import CreateTemplate from "@/components/CreateTemplate";
+import NavBar from "@/components/NavBar";
+import EditTemplate from "@/components/EditTemplate";
 export default {
   name: "Template",
-  components: {TemplateCreatePage},
+  components: {EditTemplate, NavBar, CreateTemplate},
   data() {
     return {
       fields: [
