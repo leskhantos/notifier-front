6df0350c76da663016df03b4ff7a234b0a72700b
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2020-11-09 10:54:13 +0600

    :recycle: Refactoring code.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Template from '@/pages/Template.vue'
 import GlobalParameters from '@/pages/GlobalParameters.vue'
 import History from '@/pages/History.vue'
 import Welcome from "@/pages/Login"
-import Profile from "@/pages/Profile";
-export default [
+import Profile from "@/pages/Profile"
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [
     { path: '/', component: Welcome, name: "index" },
     { path: '/templates', component: Template, name: "templates" },
     { path: '/global-parameters', component: GlobalParameters, name: "globalParameters" },
     { path: '/history', component: History, name: "history" },
     { path: '/profile', component: Profile, name: "profile" }
 
-]
+],
+    mode: 'history'
+});
