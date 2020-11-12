a41c71490ed710dd07decea583a775ba6c620e98
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2020-11-12 17:39:28 +0600

    :chart_with_upwards_trend: Added statistics for template and for all.

---
+++ src/components/TemplateStatistics.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <apexchart type="area" height="150" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TemplateStatistics",
+  data() {
+    return {
+      series: [
+          {
+        name: 'PUSH',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      },    {
+        name: 'PUSH+SMS',
+        data: [22, 10, 20, 11, 66, 29, 19]
+      }, {
+        name: 'SMS',
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }],
+      chartOptions: {
+        chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+      }
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Statistics.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <nav-bar/>
+        <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+export default {
+  name: "Statistics",
+  components: {
+    NavBar
+  },
+  data() {
+return {
+  series: [
+    {
+      name: 'PUSH',
+      data: [31, 40, 28, 51, 42, 109, 100]
+    },    {
+      name: 'PUSH+SMS',
+      data: [22, 10, 20, 11, 66, 29, 19]
+    }, {
+      name: 'SMS',
+      data: [11, 32, 45, 32, 34, 52, 41]
+    }],
+    chartOptions: {
+      chart: {
+        height: 350,
+        type: 'area'
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+      },
+      tooltip: {
+        x: {
+          format: 'dd/MM/yy HH:mm'
+        },
+      },
+    }
+    }
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
