8fa3288b0db052b7fffd6b3f89b03c26e2d56fdd
Mussa <62203153+MussaOPS@users.noreply.github.com>
2020-11-24 16:48:25 +0600

    Verstka

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,17 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #e8e8e8 ;
+  background-image: url("assets/smile.png");
+  background-repeat: no-repeat;
+  background-position: right bottom;
 }
 </style>
